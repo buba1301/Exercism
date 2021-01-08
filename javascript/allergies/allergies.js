7c36d7dc000162be37f5eb9759cd9743a55944a8
Boris Muzalevskii <bmuza57@gmail.com>
2021-01-08 18:29:12 +0300

    Allergies task is done

--- javascript/allergies/allergies.js
+++ javascript/allergies/allergies.js
@@ -2,17 +2,40 @@
 // This is only a SKELETON file for the 'Allergies' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
-
 export class Allergies {
-  constructor() {
-    throw new Error("Remove this statement and implement this function");
+  constructor(score) {
+    this.score = score;
+    this.alergiList = [
+      'eggs',
+      'peanuts',
+      'shellfish',
+      'strawberries',
+      'tomatoes',
+      'chocolate',
+      'pollen',
+      'cats',
+    ];
+    this.currentAlergiList = [];
+
+    const scoreToBinary = score.toString(2).split('').reverse();
+    let res = [];
+
+    for (let i = 0; i < scoreToBinary.length; i += 1) {
+      const binaryElement = scoreToBinary[i];
+
+      if (binaryElement === '1') {
+        res = [...res, this.alergiList[i]];
+      }
+    }
+
+    this.currentAlergiList = res.filter((i) => !!i);
   }
 
   list() {
-    throw new Error("Remove this statement and implement this function");
+    return this.currentAlergiList;
   }
 
-  allergicTo() {
-    throw new Error("Remove this statement and implement this function");
+  allergicTo(value) {
+    return this.currentAlergiList.includes(value);
   }
 }
