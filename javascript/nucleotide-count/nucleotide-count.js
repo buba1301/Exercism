c211c25ec9841c0d3500e63a541567efe4420865
Boris Muzalevskii <bmuza57@gmail.com>
2021-01-16 18:41:15 +0300

    Nucleotide-count task is done

--- javascript/nucleotide-count/nucleotide-count.js
+++ javascript/nucleotide-count/nucleotide-count.js
@@ -3,8 +3,37 @@
 // convenience to get you started writing code faster.
 //
 
+import { error } from 'console';
+
 export class NucleotideCounts {
-  static parse() {
-    throw new Error("Remove this statement and implement this function");
+  static parse(value) {
+    if (value === '') {
+      return '0 0 0 0';
+    }
+    const valueToArray = value.split('');
+
+    const nucleotidesCounteTable = valueToArray.reduce(
+      (acc, item) => {
+        const nucleotideCount = acc[item];
+
+        const nucleotides = Object.keys(acc);
+
+        const isNucleotide = nucleotides.includes(item);
+
+        if (!isNucleotide) {
+          throw new Error('Invalid nucleotide in strand');
+        }
+
+        return { ...acc, [item]: nucleotideCount + 1 };
+      },
+      {
+        A: 0,
+        C: 0,
+        G: 0,
+        T: 0,
+      }
+    );
+
+    return Object.values(nucleotidesCounteTable).join(' ');
   }
 }
